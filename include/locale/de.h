8cbd1d19a8116b30c7f156dd63665dd863986ff2
Charles Hill <chill@degreesofzero.com>
2022-07-06 15:39:38 +0100

    Lowercase language strings

--- include/locale/de.h
+++ include/locale/de.h
@@ -2,7 +2,7 @@
 #define BLESKOMAT_LOCALE_DE_H
 
 const i18n::Locale locale_de = {
-	{ "press any key", "drück einen Knopf" },
+	{ "press any key", "drück einen knopf" },
 	{ "reset", "zurücksetzen" },
 	{ "invoice", "rechnung" }
 };
